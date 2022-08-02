d8e11dd3cea681ad2d3ca93040ee08606f0b6574
Rolando90fsj <110207607+Rolando90fsj@users.noreply.github.com>
2022-08-01 22:30:06 -0400

    **update 22:30*

--- src/cl/fsj/nfoclub/collecttions/Colecciones1.java
+++ src/cl/fsj/nfoclub/collecttions/Colecciones1.java
@@ -15,9 +15,10 @@ public class Colecciones1 {
 		ArrayList<Double> decimales = new ArrayList<Double> ();
 		
 		decimales.add(32.4);
-		decimales.add(Double.parseDouble(df.format(Math.random()*1000.0)));
+		//decimales.add(Double.parseDouble(df.format(Math.random()*1000.0)));
 		decimales.add(12.456);
 		decimales.add(32.34);
+		decimales.add(62.4);
 		decimales.add(32.4);
 		
 		System.out.println("Estos son los decimales de la lista decimales: "+decimales);
@@ -34,6 +35,33 @@ public class Colecciones1 {
 		System.out.println("********************");
 		System.out.println(decimales);
 		System.out.println(decimales.size());   //Devuelve un Entero "Largo del arreglo"
+		
+		//Imprime posicion en la lista decimales
+		System.out.println(decimales.indexOf(32.4)); //0
+		System.out.println(decimales.indexOf(32.34)); //2
+		System.out.println(decimales.indexOf(32.4456)); //-1
+		System.out.println("**************SET***********");
+		// Set
+		Set<String> nicknames = new HashSet<>();
+		
+		 nicknames.add("ducksito");
+		 nicknames.add("rolando90");
+		 nicknames.add("polypleti");
+		 nicknames.add("gonzalezlillo");
+		 nicknames.add("edgamer63");
+		 nicknames.add("silvia2015");
+		 nicknames.add("sebaoteiza");
+		 nicknames.add("callita");
+		 nicknames.add("giornobanana");
+		 nicknames.add("lucianocesc");
+		 nicknames.add("felicity");
+		 nicknames.add("kernelweb");
+		 nicknames.add("cr111");
+		 nicknames.add("kessillox");
+		 
+		 
+		 System.out.println(nicknames);
+		 
 		
 		
 	}
